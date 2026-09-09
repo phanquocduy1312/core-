91b37c2f6c4f7cd4964920fd0210958aa558d881
root <root@s252.matbaows.com>
2026-09-09 22:51:50 +0700

    feat(crm): seed du lieu chuan nghiep vu cho yeu cau lien he va ban tin enewsletter

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             FoundationSeeder::class,
             BrandSeeder::class,
+            ContactAndNewsletterSeeder::class,
         ]);
     }
 }
